6c873c85567ab9721244a4126ffe1b543c202011
Yong May Yee <myong@turing.ac.uk>
2020-04-09 12:34:20 +0100

    Pare back OMOP tables to DECOVID milestones

--- PostgreSQL/populate.sql
+++ PostgreSQL/populate.sql
@@ -1,2 +1,2 @@
-COPY person(person_id,gender_concept_id,year_of_birth,race_concept_id,ethnicity_concept_id,gender_source_concept_id,race_source_concept_id,ethnicity_source_concept_id,person_source_value)
+COPY person(person_id,gender_concept_id,year_of_birth,last_updated_datetime)
 FROM '/var/lib/postgresql/csv/person.csv' DELIMITER ',' CSV HEADER;
